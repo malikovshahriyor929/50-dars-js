8f563d8a798d951049981fb8201c35b28e6a86ff
malikovshahriyor <malikovshahriyor929@gmail.com>
2024-12-07 08:52:10 +0500

    afd

---
+++ main.js
@@ -0,0 +1,65 @@
+// 1chi masala
+// let num1= [1,2,2,3]
+// let num2 = [3,2,1,2]
+// num1.sort()
+// num2.sort()
+// res = num1.join("") == num2.join("") ? true:false
+// console.log(res)
+
+// 2chi masala
+// let num = [4, 2, 9, 7, 5];
+// let res = num.sort().reverse();
+// let n = 3;
+// console.log(res[n-1]);
+
+
+
+
+
+
+
+
+
+// // 4chi masala
+// let str = ["flower","flow","fligth"]
+// res = ""
+// count= 0
+// for (let i = 0; i < str.length; i++) {
+  
+// //    console.log(str[i].split("") );
+//let spl = str[i].split("") 
+// for (let a = 0; a < spl.length; a++) {
+// console.log(spl[i])
+// // res += spl[i]
+
+// if (spl[i] == spl[i]){
+// console.log(spl[i]);
+// }
+// // else{
+// // console.log("xato")
+// // }
+// }
+    
+//    console.log(str[i]);
+//    for (let a = 0; a < str[i].length; a++) {
+    
+//    }
+   
+ 
+// }
+
+
+
+
+// 5chi masala
+// let num = [3,3,2,3,2]
+// let count = 0
+// num.forEach(element => {
+//  if (element !== count){
+// count = element
+////console.log(count);
+//  }
+//count ++
+// });
+// // let res = count
+// console.log(count=== 2 ? true:false)
